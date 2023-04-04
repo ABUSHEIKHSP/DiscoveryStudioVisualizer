6783c447e7d21e947b4ba517d25380b687aabadf
ABUSHEIKHSP <abusheikh.s.p@gmail.com>
2023-04-04 07:32:21 +0530

    initial commit

--- dsvc_pi.sh
+++
@@ -1,8 +0,0 @@
-cd home/abu/
-
-#Giving persmission to execute
-chmod 744 BIOVIA_2021.DS2021Client - 1cayjy_yP_UbkwNF_M3rm3-dqFQQEepBE.bin
-
-#Installing in folder
-./BIOVIA_2021.DS2021Client - 1cayjy_yP_UbkwNF_M3rm3-dqFQQEepBE.bin --noexec --target /home/abu/BIOVIA2020
-
